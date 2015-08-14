86579b949fe96f9109e45c9ad1570a066ce667e9
Nayan Shah <Nayan.Shah@microsoft.com>
2015-08-13 20:00:58 -0700

    Skeleton code & unit test project (including base class for T4 templates)

---
+++ src/Utils/Templates/ITemplate.cs
@@ -0,0 +1,7 @@
+﻿namespace Utils.Templates
+{
+    public interface ITemplate
+    {
+         
+    }
+}
---
+++ test/UtilsTest/Temlates/TemplateTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilsTest.Temlates
+{
+    [TestClass]
+    public class TemplateTest
+    {
+        [TestMethod]
+        public void Template_Compare()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
