2b319330d42da3c449760fabaf656e245ee5ef8f
Nayan Shah <Nayan.Shah@microsoft.com>
2015-08-14 19:07:47 -0700

    Adding appveyor.xml versioning and removing it from AssemblyInfo.cs

--- src/Utils/Properties/AssemblyInfo.cs
+++ src/Utils/Properties/AssemblyInfo.cs
@@ -7,12 +7,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Utils.Net")]
 [assembly: AssemblyDescription("Collection of utilities for .NET projects")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Utils.Net")]
 [assembly: AssemblyCopyright("Copyright © Utils.NET 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +28,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
