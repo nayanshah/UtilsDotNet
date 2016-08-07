08dda9fabc0dcd3048c44e0001d8525825838c9d
Nayan Shah <nashah@microsoft.com>
2016-08-06 18:03:18 -0700

    Extension method for creating a Graph from a collection of nodes

---
+++ src/Utils/Dgml/Extensions/GraphCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Utils.Graph.Extensions;
+
+namespace Utils.Dgml.Extensions
+{
+    public delegate Node NodeCreator<T>(T node);
+
+    public static class GraphCreator
+    {
+        /// <summary>
+        /// Creates a graph from given list of nodes. Links to nodes outside the collection are ignored
+        /// </summary>
+        /// <typeparam name="T">Custom node type</typeparam>
+        /// <param name="list">List of custom nodes</param>
+        /// <param name="nodeCreator">Delegate for creating <see cref="Node"/></param>
+        /// <param name="linkResolver">Delegate for finding links for given node</param>
+        /// <returns><see cref="Graph"/> object</returns>
+        public static Graph ToGraph<T>(this IEnumerable<T> list, NodeCreator<T> nodeCreator, NodeLinksResolver<T> linkResolver)
+        {
+            IDictionary<T, Node> nodeCache = list.ToDictionary(node => node, node => nodeCreator(node));
+
+            IEnumerable<Link> links =
+                nodeCache.Keys.SelectMany(source =>
+                    linkResolver(source)
+                        .Where(nodeCache.ContainsKey)
+                        .Select(target => new Link(nodeCache[source].Id, nodeCache[target].Id)));
+
+            return new Graph
+            {
+                Nodes = nodeCache.Values.ToArray(),
+                Links = links.ToArray(),
+            };
+        }
+    }
+}
---
+++ test/UtilsTest/Dgml/GraphCreatorTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils.Dgml;
+using Utils.Dgml.Extensions;
+using Utils.Graph.Extensions;
+
+namespace UtilsTest.Dgml
+{
+    [TestClass]
+    public class GraphCreatorTest
+    {
+        [TestMethod]
+        public void TestToGraph_ValidGraph()
+        {
+            IDictionary<string, ICollection<string>> inputGraph = new Dictionary<string, ICollection<string>>
+            {
+                { "node-1", new string[] { "node-2", "node-3", } },
+                { "node-2", new string[] { "node-4", } },
+                { "node-3", new string[] { "node-4", "node-5" } },
+                { "node-4", new string[] { } },
+                { "node-5", new string[] { } },
+            };
+
+            Graph graph = CreateGraph(inputGraph);
+
+            Assert.IsNotNull(graph);
+            MatchGraph(inputGraph, graph);
+        }
+
+        [TestMethod]
+        public void TestToGraph_ExternalLinks_Ignored()
+        {
+            IDictionary<string, ICollection<string>> inputGraph = new Dictionary<string, ICollection<string>>
+            {
+                { "node-1", new string[] { "node-2", "node-3", } },
+                { "node-2", new string[] { } },
+            };
+
+            Graph graph = CreateGraph(inputGraph);
+
+            IDictionary<string, ICollection<string>> expectedGraph = new Dictionary<string, ICollection<string>>
+            {
+                { "node-1", new string[] { "node-2", } },
+                { "node-2", new string[] { } },
+            };
+
+            Assert.IsNotNull(graph);
+            MatchGraph(expectedGraph, graph);
+        }
+
+        private Graph CreateGraph(IDictionary<string, ICollection<string>> inputGraph)
+        {
+            IList<CustomNode> nodes = inputGraph.Keys.Select(node => new CustomNode { Name = node }).ToList();
+
+            NodeLinksResolver<CustomNode> resolver =
+                node => inputGraph[node.Name]
+                            .Select(
+                               link => nodes.SingleOrDefault(n => link == n.Name) ?? new CustomNode { Name = link });
+
+            NodeCreator<CustomNode> creator = node => new Node(node.Name);
+
+            return nodes.ToGraph(creator, resolver);
+        }
+
+        private void MatchGraph(IDictionary<string, ICollection<string>> expectedGraph, Graph actualGraph)
+        {
+            Assert.AreEqual(expectedGraph.Keys.Count, actualGraph.Nodes.Length);
+            Assert.AreEqual(expectedGraph.Values.SelectMany(v => v).Count(), actualGraph.Links.Length);
+
+            foreach (string nodeName in expectedGraph.Keys)
+            {
+                Assert.IsNotNull(actualGraph.Nodes.Single(n => n.Id == nodeName));
+                foreach (string link in expectedGraph[nodeName])
+                {
+                    Assert.IsNotNull(actualGraph.Links.Single(node => node.Source == nodeName && node.Target == link));
+                }
+            }
+        }
+
+        private class CustomNode
+        {
+            public string Name;
+        }
+    }
+}
